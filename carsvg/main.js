6c2b9e695878954a43205dd0fec663571482502e
andriykobarov1213 <48562388+andriykobarov1213@users.noreply.github.com>
2019-06-20 11:28:47 +0300

    change

---
+++ carsvg/main.js
@@ -0,0 +1,20 @@
+var a = document.getElementsByTagName("path");
+	
+function f1 () {
+	alert("Бампер");
+}
+function f2 () {
+	alert("Колесо");
+}
+function f3 () {
+	alert("Окно");
+}
+function f4 () {
+	alert("Дверь");
+}
+function f5 () {
+	alert("Фары");
+}
+function f6 () {
+	alert("Решетка радиатора");
+}
