af2c37138266e045b8dc5e7cd292e2c514f89313
andriykobarov1213 <48562388+andriykobarov1213@users.noreply.github.com>
2019-09-18 22:54:44 +0300

    ;
    5

--- lendos/js/main.js
+++ lendos/js/main.js
@@ -5,7 +5,31 @@ var menu = document.getElementById("menu-left");
 var menu2 = document.getElementById("menu-right");
 var line1 = document.getElementById("line1");
 var line2 = document.getElementById("line2");
-var a = 0;
+
+var link1 = document.getElementById("link1");
+var link2 = document.getElementById("link2");
+var link3 = document.getElementById("link3");
+var link4 = document.getElementById("link4");
+var link5 = document.getElementById("link5");
+
+
+
+link1.onmouseover = function(){
+	menu.style.backgroundImage = 'url(img/addi-menu.png)'
+
+}
+link2.onmouseover = function(){
+	menu.style.backgroundImage = 'url(img/Nike.jpg)'
+}
+link3.onmouseover = function(){
+	menu.style.backgroundImage = 'url(img/reebok.jpg)'
+}
+link4.onmouseover = function(){
+	menu.style.backgroundImage = 'url(img/puma.jpg)'
+}
+link5.onmouseover = function(){
+	menu.style.backgroundImage = 'url(img/new.jpg)'
+}
 
 open.onclick = function(){
 	menu.style.display = "block";
