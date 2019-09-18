b9c398fa253e60bddf1d95070c4ffd6778598af0
andriykobarov1213 <48562388+andriykobarov1213@users.noreply.github.com>
2019-09-18 15:50:57 +0300

    h

--- lendos/js/main.js
+++ lendos/js/main.js
@@ -1,9 +1,25 @@
 var open = document.getElementById("open");
+var open2 = document.getElementById("open2");
 var close = document.getElementById("close");
 var menu = document.getElementById("menu-left");
+var menu2 = document.getElementById("menu-right");
+var line1 = document.getElementById("line1");
+var line2 = document.getElementById("line2");
+var a = 0;
 
 open.onclick = function(){
 	menu.style.display = "block";
+}
+open2.onclick = function(){
+	menu2.style.display = "block";
+	open2.style.display = "none";
+	line1.style.display = "block";
+	a = a + 1;
+}
+line1.onclick = function(){
+	menu2.style.display = "none";
+	open2.style.display = "block";
+	line1.style.display = "none";
 }
 close.onclick = function(){
 	menu.style.display = "none";
