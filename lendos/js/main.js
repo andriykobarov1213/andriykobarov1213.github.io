321b2596159493efa0115656f4ee8c333cb28406
andriykobarov1213 <48562388+andriykobarov1213@users.noreply.github.com>
2019-09-18 15:00:15 +0300

    5

---
+++ lendos/js/main.js
@@ -0,0 +1,11 @@
+var open = document.getElementById("open");
+var close = document.getElementById("close");
+var menu = document.getElementById("menu-left");
+
+open.onclick = function(){
+	menu.style.display = "block";
+}
+close.onclick = function(){
+	menu.style.display = "none";
+} 
+alert("ku"); 
